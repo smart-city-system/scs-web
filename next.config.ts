36f74f4ec11608d1f676de47bebe6af0954dd491
cuongbq <erikbuidt@gmail.com>
2025-09-17 18:34:43 +0800

    chore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  env: {
+    NEXT_PUBLIC_USER_ENDPOINT: process.env.NEXT_PUBLIC_USER_ENDPOINT,
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    NEXT_PUBLIC_CAMERA_ENDPOINT: process.env.NEXT_PUBLIC_CAMERA_ENDPOINT,
+    NEXT_PUBLIC_NOTIFICATION_ENDPOINT: process.env.NEXT_PUBLIC_NOTIFICATION_ENDPOINT,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
